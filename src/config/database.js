0f52783fb32e69277ed536a06b92e73777148b25
Evanio Tech <evanioshark@gmail.com>
2025-02-07 07:22:18 -0300

    instalado sequelize

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {  
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'secrect',
+    database: 'teste-dominando-nodejs',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+};
