97c682fe0ea3ee75692f1b0d81fad53f8aa4c669
Evanio Tech <evanioshark@gmail.com>
2025-02-14 11:51:59 -0300

    colocando asociate

--- src/database/index.js
+++ src/database/index.js
@@ -12,10 +12,14 @@ class Database {
     constructor() {
        this.connection = new Sequelize(config);
         this.init();
+        this.associate();
     }
 
     init() {
         models.map(model => model.init(this.connection));
+    }
+    associate() {
+        models.map(model => model.associate && model.associate(this.connection.models));
     }
 }   
 
