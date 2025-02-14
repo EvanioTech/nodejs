d1e5441c2a5d86eb2ec99d8761024602078961e8
Evanio Tech <evanioshark@gmail.com>
2025-02-14 09:25:31 -0300

    usando op

--- src/playground.js
+++ src/playground.js
@@ -1,11 +1,23 @@
 import './database';
 
 import Customer from './app/models/Customer';
+import { Op } from 'sequelize';
 
 
 class Playground {
     static async run() {
-       const customer = await Customer.findAll();
+        // comando para buscar id especifico
+       // const customer = await Customer.findByPk(1);
+       const customer = await Customer.findAll({
+            where: {
+                status: {
+                    [Op.ne]: "inactive"
+                }
+                
+                
+            }, 
+            
+       });
             
 
 
